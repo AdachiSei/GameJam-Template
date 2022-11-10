28c22f86db7a0f83c99c42c3decf75bdfeb6ae5c
DESKTOP-P0CK6VC\Sei Adachi <sei.dance.dream@gmail.com>
2022-11-11 01:01:05 +0900

    計算機制作中

---
+++ Assets/Template/Calculator.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Calculator
+{
+    static void A()
+    {
+        
+    }
+
+    public static T RandomT<T>(T num) where T : struct
+    {
+        return num;
+    }
+}
