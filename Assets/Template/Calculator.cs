36e190a8a52b5a26c369f42cb867609c2a19e950
DESKTOP-P0CK6VC\Sei Adachi <sei.dance.dream@gmail.com>
2022-11-11 20:31:59 +0900

    計算機制作中

--- Assets/Template/Calculator.cs
+++ Assets/Template/Calculator.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public static class Calculator
 {
-    static void A()
+    public static TValue RandomT<T, TValue>(TValue a, TValue b) where T : IRandom<TValue>, new()
     {
-        
+        return new T().RandomNum(a, b);
     }
 
-    public static T RandomT<T>(T num) where T : struct
+    public static TValue B<TValue>(int a) where TValue : Testoo<TValue>,new()
     {
-        return num;
+        return new TValue().A(a);
     }
+}
+
+public interface Testoo<T>
+{
+    public T A(int a);
+}
+
+public struct RandomInt : IRandom<int>
+{
+    public int RandomNum(int min, int max) => Random.Range(min, max);
+}
+
+public struct RandomFloat : IRandom<float>
+{
+    public float RandomNum(float min, float max) => Random.Range(min, max);
 }
---
+++ Assets/Template/Scripts/Interface/IRandom.cs
@@ -0,0 +1,8 @@
+/// <summary>
+/// 計算機に必要なinterface
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public interface IRandom<T>
+{
+    T RandomNum(T a, T b);
+}
