897512bff9815ec76a006a24f204d2ca75dedbc6
DESKTOP-P0CK6VC\Sei Adachi <sei.dance.dream@gmail.com>
2022-11-14 16:04:49 +0900

    計算機修正

--- Assets/Template/Scripts/Calculator.cs
+++ Assets/Template/Scripts/Calculator.cs
@@ -6,7 +6,14 @@ using UnityEngine;
 
 public static class Calculator
 {
-    public static int Probability(int[] num)
+    const float MAX_VALUE = 100f;
+
+    /// <summary>
+    /// ガチャのような関数
+    /// </summary>
+    /// <param name="num">確率</param>
+    /// <returns>Index</returns>
+    public static int RandomIndex(float[] num)
     {
         float[] probability = { };
         float sum = num.Sum();
@@ -16,12 +23,12 @@ public static class Calculator
         {
             for (int count = 0; count < limitCount; count++)
             {
-                probability[index] += num[count] * 100f / sum;
+                probability[index] += num[count] * MAX_VALUE / sum;
             }
             Debug.Log(index + "番目 " + probability[index]);
             limitCount++;
         }
-        var random = UnityEngine.Random.Range(0f, 100f);
+        var random = UnityEngine.Random.Range(0f, MAX_VALUE);
         Debug.Log("乱数 " + random);
         for (int i = 0; i < probability.Length; i++)
         {
@@ -34,7 +41,12 @@ public static class Calculator
         return 0;
     }
 
-    public static int Probability(float[] num)
+    /// <summary>
+    /// ガチャのような関数
+    /// </summary>
+    /// <param name="num">確率</param>
+    /// <returns>Index</returns>
+    public static int RandomIndex(int[] num)
     {
         float[] probability = { };
         float sum = num.Sum();
@@ -44,21 +56,43 @@ public static class Calculator
         {
             for (int count = 0; count < limitCount; count++)
             {
-                probability[index] += num[count] * 100f / sum;
+                probability[index] += num[count] * MAX_VALUE / sum;
             }
-             Debug.Log(index + "番目 " + probability[index]);
+            Debug.Log(index + "番目 " + probability[index]);
             limitCount++;
         }
-        var random = UnityEngine.Random.Range(0f,100f);
+        var random = UnityEngine.Random.Range(0f, MAX_VALUE);
         Debug.Log("乱数 " + random);
         for (int i = 0; i < probability.Length; i++)
         {
-            if(probability[i] > random)
+            if (probability[i] > random)
             {
                 Debug.Log("結果は" + i);
                 return i;
-            }    
+            }
         }
         return 0;
+    }
+
+    /// <summary>
+    /// ガチャのような関数
+    /// </summary>
+    /// <param name="num">確率</param>
+    /// <returns>Index</returns>
+    public static int RandomIndex(IReadOnlyList<float> num)
+    {
+        var newNum = num.ToArray();
+        return RandomIndex(newNum);
+    }
+
+    /// <summary>
+    /// ガチャのような関数
+    /// </summary>
+    /// <param name="num">確率</param>
+    /// <returns>Index</returns>
+    public static int RandomIndex(IReadOnlyList<int> num)
+    {
+        var newNum = num.ToArray();
+        return RandomIndex(newNum);
     }
 }
