7e1311755711a35a0661c9555c369d4996c83a95
DESKTOP-P0CK6VC\Sei Adachi <sei.dance.dream@gmail.com>
2022-11-23 08:27:39 +0900

    オブジェクトプールを生成

---
+++ Assets/Template/Scripts/Manager/ObjectPool.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// オブジェクトプール用のScript
+/// </summary>
+public class ObjectPool : MonoBehaviour
+{
+    /// <summary>
+    /// オブジェクトを生成してList格納する関数
+    /// </summary>
+    /// <typeparam name="T">プーリングしたいオブジェクトの型</typeparam>
+    /// <param name="pools">オブジェクトを格納しているList</param>
+    /// <param name="prefab">オブジェクトのプレファブ</param>
+    /// <param name="count">生成するオブジェクトの数</param>
+    /// <param name="parent">オブジェクトを格納する親オブジェクト</param>
+    public static void CreatePool<T>(IReadOnlyList<T> pools,T prefab, int count,Transform parent = null) where T : class
+    {
+        var castedList = pools as List<T>;
+        for (int i = 0; i < count; i++)
+        {
+            var newPool = Instantiate(prefab as GameObject);
+            newPool.transform.SetParent(parent?.transform);
+            var pool = newPool as T;
+            castedList.Add(pool);
+        }
+    }
+
+    /// <summary>
+    /// 非アクティブのオブジェクトを探してアクティブ化してくれる関数
+    /// </summary>
+    /// <typeparam name="T">プーリングしたいオブジェクトの型</typeparam>
+    /// <param name="pools">オブジェクトを格納しているList</param>
+    /// <param name="prefab">オブジェクトのプレファブ</param>
+    /// <param name="parent">オブジェクトを格納する親オブジェクト</param>
+    /// <returns>非アクティブだったオブジェクト</returns>
+    public static T UseObject<T>(IReadOnlyList<T> pools,T prefab ,Transform parent = null) where T : class
+    {
+        var poolObjects = pools as List<GameObject>;
+        for (int index = 0; index < poolObjects.Count; index++)
+        {
+            if(poolObjects[index].activeSelf == false)
+            {
+                poolObjects[index].SetActive(true);
+                return pools[index];
+            }
+        }
+        var newPool = Instantiate(prefab as GameObject);
+        newPool.transform.SetParent(parent?.transform);
+        var pool = newPool as T;
+        var castedList = pools as List<T>;
+        castedList.Add(pool);
+        return pool;
+    }
+}
