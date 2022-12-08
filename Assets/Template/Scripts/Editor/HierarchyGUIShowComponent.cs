926ff07903ad37d0eb65d7192acef84fecb909bd
DESKTOP-P0CK6VC\Sei Adachi <sei.dance.dream@gmail.com>
2022-12-08 23:58:22 +0900

    アイコン表示のエディター拡張修正

--- Assets/Template/Scripts/Editor/HierarchyGUIShowComponent.cs
+++ Assets/Template/Scripts/Editor/HierarchyGUIShowComponent.cs
@@ -5,57 +5,60 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
+/// <summary>
+/// ヒエラルキー上のゲームオブジェクトの横に
+/// つけているコンポーネントのアイコンを表示するエディター拡張
+/// </summary>
 public static class HierarchyGUIShowComponent
 {
+    #region Const Member
+
     /// <summary>
     /// アイコンのサイズ
     /// </summary>
     private const int ICON_SIZE = 16;
+
+    private const int OFFSET = 1;
+
+    #endregion
+
+    #region Unity Methods
 
     [InitializeOnLoadMethod]
     private static void Initialize()
     {
         EditorApplication.hierarchyWindowItemOnGUI += OnGUI;
     }
-    
+
     private static void OnGUI(int instanceID, Rect selectionRect)
     {
         // instanceID をオブジェクト参照に変換
         var go = EditorUtility.InstanceIDToObject(instanceID) as GameObject;
-        if (go == null)
-        {
-            return;
-        }
+        if (go == null) return;
 
         // オブジェクトが所持しているコンポーネント一覧を取得
         var components = go.GetComponents<Component>();
         List<Component> componentsList = new();
-        foreach (var i in components)
+        foreach (var component in components)
         {
             bool[] isType =
             {
-                i.GetType() == typeof(Transform),
-                i.GetType() == typeof(RectTransform),
-                i.GetType() == typeof(CanvasRenderer),
-                i.GetType() == typeof(CanvasScaler),
-                i.GetType() == typeof(StandaloneInputModule),
-                i.GetType() == typeof(GraphicRaycaster)
+                component.GetType() == typeof(Transform),
+                component.GetType() == typeof(RectTransform),
+                component.GetType() == typeof(CanvasRenderer),
+                component.GetType() == typeof(CanvasScaler),
+                component.GetType() == typeof(StandaloneInputModule),
+                component.GetType() == typeof(GraphicRaycaster)
             };
 
-            for (int j = 0; j < isType.Length; j++)
+            for (int index = 0; index < isType.Length; index++)
             {
-                if (isType[j])
-                {
-                    break;
-                }
-                if(j == isType.Length - 1)componentsList.Add(i);
+                if (isType[index]) break;
+                if (index == isType.Length - OFFSET) componentsList.Add(component);
                 continue;
             }
         }
-        if (componentsList.Count == 0)
-        {
-            return;
-        }
+        if (componentsList.Count == 0) return;
 
         selectionRect.x = selectionRect.xMax - ICON_SIZE * componentsList.Count;
         selectionRect.width = ICON_SIZE;
@@ -68,4 +71,6 @@ public static class HierarchyGUIShowComponent
             selectionRect.x += ICON_SIZE;
         }
     }
+
+    #endregion
 }
