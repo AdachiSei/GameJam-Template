e00fd73fce2e5e1914c1fe42f7c4faceba31fdf8
DESKTOP-P0CK6VC\Sei Adachi <sei.dance.dream@gmail.com>
2022-12-26 23:52:12 +0900

    テンプレートファイル修正

--- Assets/Template/Scripts/Editor/MenuItemSettings.cs
+++ Assets/Template/Scripts/Editor/MenuItemSettings.cs
@@ -10,8 +10,8 @@ namespace Template.Plugins
     {
         private const int SCRIPT_PRIOPRITY = 10;
 
-        private const string PLUGIN_PATH = "Assets/Plugins/ScriptFilesFromTemplate/";
-        private const string NEW_FILENAME = "NewFile.cs";
+        private const string PLUGIN_PATH = "Assets/Template/Files/";
+        private const string NEW_FILENAME = "NewScript.cs";
 
         private const string MENU_ITEM_ROOT = "Assets/Create/Add Script Files/";
 
@@ -20,19 +20,28 @@ namespace Template.Plugins
         private const string MonoBehaviourWithUniRx = "MonoBehaviour (UniRx)";
 
         [MenuItem(MENU_ITEM_ROOT + SimpleScript, priority = SCRIPT_PRIOPRITY)]
-        private static void CreateSimpleScript() => CreateScriptFile("SimpleScript.txt", NEW_FILENAME);
+        private static void CreateSimpleScript()
+        {
+            CreateScriptFile("SimpleScript.cs", NEW_FILENAME);
+        }
 
         [MenuItem(MENU_ITEM_ROOT + MonoBehaviour, priority = SCRIPT_PRIOPRITY)]
-        private static void CreateMonoBehaviour() => CreateScriptFile("TemplateMonoBehaviour.txt", NEW_FILENAME);
+        private static void CreateMonoBehaviour()
+        {
+            CreateScriptFile("TemplateMonoBehaviour.txt", NEW_FILENAME);
+        }
 
         [MenuItem(MENU_ITEM_ROOT + MonoBehaviourWithUniRx, priority = SCRIPT_PRIOPRITY)]
-        private static void CreateMonoBehaviourWithUniRx() => CreateScriptFile("TemplateMonoBehaviourWithUniRx.txt", NEW_FILENAME);
+        private static void CreateMonoBehaviourWithUniRx()
+        {
+            CreateScriptFile("TemplateMonoBehaviourWithUniRx.txt", NEW_FILENAME);
+        }
 
         private static void CreateScriptFile(string templateFileName, string newFileName)
         {
             ProjectWindowUtil
                 .CreateScriptAssetFromTemplateFile
-                    (System.IO.Path.Combine(PLUGIN_PATH, $"Templates/{templateFileName}"), newFileName);
+                    (System.IO.Path.Combine(PLUGIN_PATH, $"{templateFileName}"), newFileName);
         }
     }
 }
