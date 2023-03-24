6d4016226d151db18d827fe59ea6c137ff0f37fe
DESKTOP-P0CK6VC\Sei Adachi <sei.dance.dream@gmail.com>
2023-03-24 23:54:34 +0900

    保存先を移動させた

---
+++ Assets/Template/Scripts/Editor/AssetPostprocessor/AudioPostprocessor.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// オーディオクリップをインポートした時にオーディオの設定を変更する
+/// https://amagamina.jp/blog/audio-clip/
+/// https://qiita.com/ptkyoku/items/84b62cfbb4282a7cd7e6
+/// https://kan-kikuchi.hatenablog.com/entry/AudioSettings
+/// </summary>
+public class AudioPostprocessor : AssetPostprocessor
+{
+    #region Unity Methods
+
+    private void OnPostprocessAudio(AudioClip clip)
+    {
+        var audioImporter = assetImporter as AudioImporter;
+
+        //初回インポートのみに制限
+        if (!audioImporter.importSettingsMissing) return;
+
+        SetAudioImporter(clip, audioImporter);
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    /// <summary>
+    /// 音楽や効果音を適した設定に変更する関数
+    /// </summary>
+    private void SetAudioImporter(AudioClip clip, AudioImporter importer)
+    {
+        //DefaultのAudioImporterSampleSettings取得
+        var settings = importer.defaultSampleSettings;
+
+        var isBGM = clip.length >= BGMLengthData.BGMLength;
+        if (isBGM)//音楽
+        {
+            //ロードしながら再生を行うので、メモリをほんの少ししか使わない
+            settings.loadType = AudioClipLoadType.Streaming;
+        }
+        else//効果音
+        {
+            //ファイルサイズがおよそ半分になる
+            importer.forceToMono = true;
+            //展開速度が早い、CPUの負荷を抑えられる
+            settings.compressionFormat = AudioCompressionFormat.ADPCM;
+        }
+        //変更を反映
+        importer.defaultSampleSettings = settings;
+    }
+
+    #endregion
+}
---
+++ Assets/Template/Scripts/Struct/Value.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public struct Value<T>
+{
+    #region Properties
+
+    public T MinValue => _minValue;
+    public T MaxValue => _maxValue;
+
+    #endregion
+
+    #region Inspector Variables
+
+    [SerializeField]
+    [Header("小さい値")]
+    private T _minValue;
+
+    [SerializeField]
+    [Header("大きい値")]
+    private T _maxValue;
+
+    #endregion
+
+    #region Public Methods
+
+    public void SetValue(T minValue, T maxValue)
+    {
+        _minValue = minValue;
+        _maxValue = maxValue;
+    }
+
+    #endregion
+}
